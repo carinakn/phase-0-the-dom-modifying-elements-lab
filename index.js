4eff3fe1474c8057e0c2da0a5fac9e737ace13cd
carinakn <carinaknics@gmail.com>
2023-10-22 16:52:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "CARINA is the champion";
